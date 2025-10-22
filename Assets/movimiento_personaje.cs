142e2ccb50a36787653f8effc010ee27cacfb87b
Nicolas Spinoza <fafuster33@gmail.com>
2025-10-22 14:32:16 -0300

    Initial commit

---
+++ Assets/movimiento_personaje.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+ 
+public class movimiento_personaje : MonoBehaviour
+{
+ 
+    public float Velocidad = 5f;
+    public float FuerzaSalto = 8f;
+    private Rigidbody2D rb;
+    private bool puedeSaltar = false;
+ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(Velocidad * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(Velocidad * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) &&   puedeSaltar)    
+        {
+            rb.AddForce(new Vector2(0f, FuerzaSalto), ForceMode2D.Impulse);
+        }
+    }
+ 
+    void OnCollisionEnter2D(Collision2D objeto)
+    {
+        if (objeto.gameObject.tag == "Floor")
+        {
+            Debug.Log("No puede saltar");
+            puedeSaltar = true;
+        }
+    }
+ 
+     void OnCollisionExit2D(Collision2D objeto)
+    {
+        if(objeto.gameObject.tag == "Floor")
+        {
+            Debug.Log("Puede saltar");
+            puedeSaltar = false;
+        }
+    }
+}

3cf39ef839be956c506a6b4c62bdf8b7cef553dd
Nicolas Spinoza <fafuster33@gmail.com>
2025-10-29 15:57:47 -0300

    enemigos

--- Assets/DañoPersonaje.cs
+++ Assets/DañoPersonaje.cs
@@ -5,21 +5,30 @@ public class DañoPersonaje : MonoBehaviour
 {
  
     public Image barraVida;
- 
+    private bool estaVivo = true;
     public float vida = 1f;
  
     private bool recibiendoDaño = false;
+    private bool muerte = false;
+
+    public GameObject puntoControl;
  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("GenerarDaño", 0f, 1f);
+        InvokeRepeating("GenerarDaño", 0f, 0.6f);
     }
  
     void Update()
     {
         barraVida.fillAmount = vida;
- 
+
+        if (vida <= 0f && estaVivo)
+        {
+            estaVivo = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+            Invoke("reaparecer", 3);
+        }
  
     }
  
@@ -28,6 +37,11 @@ public class DañoPersonaje : MonoBehaviour
         if (other.gameObject.tag == "enemigo")
         {
             recibiendoDaño = true;
+        }
+
+        if (other.gameObject.tag == "pinches")
+        {
+            muerte = true;
         }
     }
  
@@ -46,13 +60,27 @@ public class DañoPersonaje : MonoBehaviour
             recibiendoDaño = false;
         }
     }
- 
+
     // Update is called once per frame
     void GenerarDaño()
     {
-        if(recibiendoDaño)
+        if (recibiendoDaño)
         {
             vida -= 0.2f;
         }
+
+        if (muerte)
+        {
+            vida -= 1f;
+        }
+    }
+    
+    void reaparecer()
+    {
+        transform.position = puntoControl.transform.position;
+        GetComponent<SpriteRenderer>().enabled = true;
+        vida = 1f;
+        muerte = false;
+        estaVivo = true;
     }
 }
